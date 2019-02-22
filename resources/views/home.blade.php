a506baa129e39ceb340178aa12e311958f36abef
mobydickens <steadmegan@gmail.com>
2019-02-22 13:08:37 -0700

    starting first actual project with Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+  <div class='container'>
+    <h4>Do you have a list of books to read, but aren't sure which should be next? You've come to the right place.</h4>
+  </div>
+
+
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Next Book</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+      html, body {
+          background-color: #fff;
+          color: #636b6f;
+          font-family: 'Nunito', sans-serif;
+          font-weight: 200;
+          height: 100vh;
+          margin: 20px;
+      }
+      .container {
+        margin: 10px;
+      
+    </style>
+  </head>
+
+  <body>
+    <h1>My Next Book</h1>
+    @yield('content')
+  </body>
+</html>
